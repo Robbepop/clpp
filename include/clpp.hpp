760e25be498c72ad37b907ccb3eada5e9d125e5f
Robin Freyler <RobinFreyler@web.de>
2015-06-13 14:55:36 +0200

    added undef for REQUIRES macro

--- include/clpp.hpp
+++ include/clpp.hpp
@@ -7,4 +7,11 @@
 #include "clpp/device.hpp"
 #include "clpp/context.hpp"
 
+//===========================================================
+// Undefine defined macros to not invalidate other macros
+// from other libraries and/or components.
+//===========================================================
+
+#undef REQUIRES // defined in "utility/type_traits.hpp"
+
 #endif
