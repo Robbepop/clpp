41622f70607c827b303b26921de08ce98a62ace2
Robin Freyler <RobinFreyler@web.de>
2015-07-14 13:37:00 +0200

    removed some MemObjectType enums for OpenCL 1.2 and 1.1

--- include/clpp/mem_object_type.hpp
+++ include/clpp/mem_object_type.hpp
@@ -5,14 +5,18 @@
 
 namespace cl {
 	enum class MemObjectType {
-		buffer        = CL_MEM_OBJECT_BUFFER,
-		pipe          = CL_MEM_OBJECT_PIPE,
-		image2D       = CL_MEM_OBJECT_IMAGE2D,
-		image3D       = CL_MEM_OBJECT_IMAGE3D,
-		image2DArray  = CL_MEM_OBJECT_IMAGE2D_ARRAY,
-		image1D       = CL_MEM_OBJECT_IMAGE1D,
-		image1DArray  = CL_MEM_OBJECT_IMAGE1D_ARRAY,
-		image1DBuffer = CL_MEM_OBJECT_IMAGE1D_BUFFER
+		  buffer        = CL_MEM_OBJECT_BUFFER
+	#if defined(CL_VERSION_2_0)
+		, pipe          = CL_MEM_OBJECT_PIPE
+	#endif // defined(CL_VERSION_2_0)
+		, image2D       = CL_MEM_OBJECT_IMAGE2D
+		, image3D       = CL_MEM_OBJECT_IMAGE3D
+	#if defined(CL_VERSION_1_2)
+		, image2DArray  = CL_MEM_OBJECT_IMAGE2D_ARRAY
+		, image1D       = CL_MEM_OBJECT_IMAGE1D
+		, image1DArray  = CL_MEM_OBJECT_IMAGE1D_ARRAY
+		, image1DBuffer = CL_MEM_OBJECT_IMAGE1D_BUFFER
+	#endif // defined(CL_VERSION_1_2)
 	};
 }
 
